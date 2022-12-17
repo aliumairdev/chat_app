4b7609ce4cd1f833aaf2131d43823464f4856160
aliumairdev <aliumair.dev@gmail.com>
2022-12-17 22:42:06 +0500

    devise added

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  root to: 'pages#home'
+  devise_for :users
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  devise_scope :user do
+    get 'users', to: 'devise/sessions#new'
+  end
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
